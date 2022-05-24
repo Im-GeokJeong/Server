8b27a93318a4722f9e0f252056ddf07f9b12dec0
SeongukBaek <bsu6235@gmail.com>
2022-05-24 14:40:50 +0900

    #1 establish: establish server & set up api

---
+++ src/main/java/IGJ/imgeokjeong/farm/dto/FarmMachineRequest.java
@@ -0,0 +1,9 @@
+package IGJ.imgeokjeong.farm.dto;
+
+import lombok.Getter;
+
+@Getter
+public class FarmMachineRequest {
+    int pageNo;
+    int numOfRows;
+}
---
+++ src/test/java/IGJ/imgeokjeong/ImgeokjeongApplicationTests.java
@@ -0,0 +1,13 @@
+package IGJ.imgeokjeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgeokjeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
