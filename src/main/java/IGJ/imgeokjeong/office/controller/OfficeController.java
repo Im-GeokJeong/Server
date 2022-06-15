8a5320642b15764176aace0ecfca9a4cb9678b14
ONE <sodlapdlf13@gmail.com>
2022-06-15 14:31:26 +0900

    #8 feat: add swagger

--- src/main/java/IGJ/imgeokjeong/office/controller/OfficeController.java
+++ src/main/java/IGJ/imgeokjeong/office/controller/OfficeController.java
@@ -2,6 +2,7 @@ package IGJ.imgeokjeong.office.controller;
 
 import IGJ.imgeokjeong.errors.response.CommonResponse;
 import IGJ.imgeokjeong.office.service.OfficeService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +13,13 @@ public class OfficeController {
 
     private final OfficeService officeService;
 
+    @Operation(summary = "get office list", description = "경북 지역의 임대 사업소 리스트")
     @GetMapping("/list")
     public CommonResponse officeList() {
         return officeService.list();
     }
 
+    @Operation(summary = "get office info", description = "임대 사업소 ID로 상세 정보")
     @GetMapping("/{officeId}")
     public CommonResponse officeDetails(@PathVariable("officeId") Long id) {
         return officeService.detail(id);
