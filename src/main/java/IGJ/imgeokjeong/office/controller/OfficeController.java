517988a692aed701217d10d35c920f42d5fbea0e
SeongukBaek <bsu6235@gmail.com>
2022-07-14 17:17:33 +0900

    #14 feature: add offices by machines and current location

--- src/main/java/IGJ/imgeokjeong/office/controller/OfficeController.java
+++ src/main/java/IGJ/imgeokjeong/office/controller/OfficeController.java
@@ -1,10 +1,14 @@
 package IGJ.imgeokjeong.office.controller;
 
 import IGJ.imgeokjeong.errors.response.CommonResponse;
+import IGJ.imgeokjeong.office.dto.NearestRequest;
 import IGJ.imgeokjeong.office.service.OfficeService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/office")
@@ -29,5 +33,11 @@ public class OfficeController {
     @GetMapping("/machine/{cropName}")
     public CommonResponse machineForCropList(@PathVariable("cropName") String cropName) {
         return officeService.neededForCropList(cropName);
+    }
+
+    @Operation(summary = "get nearest office list has machines", description = "현 위치에서 가장 가깝고 농기계를 보유 중인 사업소 리스트")
+    @PostMapping("/offices")
+    public CommonResponse nearestOfficeList(@RequestBody @Valid NearestRequest nearestRequest) {
+        return officeService.nearestList(nearestRequest);
     }
 }
